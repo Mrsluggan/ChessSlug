59f1dfbeaff9ce64fce86a6f907c3826ba517c03
mrsluggan <ericflyger@gmail.com>
2024-11-02 11:37:30 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './Style/index.css';
 import App from './App';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <HashRouter>
+  <Router>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>
+  </Router>
 );
 
